5a56f6cefa58f3e5b1203d250df7b9eb0908b03f
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-07-25 23:57:18 +0500

    working on Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <h1>Adeel</h1>
   )
 }
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,13 +1,15 @@
-"use client"
+"use client";
 
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
+  { name: "Signals", href: "#", current: false },
+  { name: "Latest News", href: "#", current: false },
+  { name: "Blog", href: "#", current: false },
+  { name: "WatchList", href: "#", current: false },
+  { name: "Signals", href: "#", current: false },
   { name: "Calendar", href: "#", current: false },
 ];
 
